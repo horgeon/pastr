ce0d7df1a7942ac344ba276fae37e3de5012e406
Horgeon <me@horgeon.fr>
2024-06-10 22:31:56 +0000

    Put content in individual files, using raw bytes and proper type detection

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
 	"io"
@@ -10,37 +9,64 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"strconv"
-	"strings"
 	"unicode"
 )
 
-const DbFile = "data.db"
+const maxUrlLength = 32768 // 16x max length defined in RFC, should be good enough to handle Unicode URLs
 
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
+var dataPath = "data"
 var useForwardedHeaders = false
 var keyLength = 4
 
 func init() {
-	newUseForwardedHeaders, err := strconv.ParseBool(os.Getenv("PASTR_USE_FORWARDED_HEADERS"))
+	envValue := os.Getenv("PASTR_DATA_PATH")
+	if envValue != "" {
+		dataPath = envValue
+	}
+	// Get absolute data path
+	newDataPath, err := filepath.Abs(dataPath)
 	if err == nil {
-		useForwardedHeaders = newUseForwardedHeaders
+		// Path is well-formed, check if it exists
+		if _, err := os.Stat(newDataPath); !os.IsNotExist(err) {
+			dataPath = newDataPath
+		} else {
+			// If path does not exist, attempt to create it
+			err := os.MkdirAll(newDataPath, os.ModePerm)
+			if err == nil {
+				dataPath = newDataPath
+			} else {
+				log.Panic("Invalid data path: \"", newDataPath, "\": ", err)
+			}
+		}
 	} else {
-		envValue := os.Getenv("PASTR_USE_FORWARDED_HEADERS")
-		if envValue != "" {
-			log.Print("Invalid PASTR_USE_FORWARDED_HEADERS: \"", envValue, "\"")
+		log.Panic("Invalid data path: \"", newDataPath, "\": ", err)
+	}
+
+	envValue2 := os.Getenv("PASTR_USE_FORWARDED_HEADERS")
+	if envValue2 != "" {
+		newUseForwardedHeaders, err := strconv.ParseBool(envValue2)
+		if err == nil {
+			useForwardedHeaders = newUseForwardedHeaders
+		} else {
+			log.Print("Invalid PASTR_USE_FORWARDED_HEADERS: \"", envValue2, "\"")
 		}
 	}
-	newKeyLength, err := strconv.Atoi(os.Getenv("PASTR_KEY_LENGTH"))
-	if err == nil && newKeyLength >= 4 && newKeyLength <= 12 {
-		keyLength = newKeyLength
-	} else {
-		envValue := os.Getenv("PASTR_KEY_LENGTH")
-		if envValue != "" {
-			log.Print("Invalid PASTR_KEY_LENGTH: \"", envValue, "\"")
+
+	envValue3 := os.Getenv("PASTR_KEY_LENGTH")
+	if envValue3 != "" {
+		newKeyLength, err := strconv.Atoi(envValue3)
+		if err == nil && newKeyLength >= 4 && newKeyLength <= 12 {
+			keyLength = newKeyLength
+		} else {
+			log.Print("Invalid PASTR_KEY_LENGTH: \"", envValue3, "\"")
 		}
 	}
+
+	log.Print("Configuration value PASTR_DATA_PATH = ", dataPath)
 	log.Print("Configuration value PASTR_USE_FORWARDED_HEADERS = ", useForwardedHeaders)
 	log.Print("Configuration value PASTR_KEY_LENGTH = ", keyLength)
 }
@@ -50,7 +76,7 @@ func main() {
 		if r.Method == "POST" {
 			defer r.Body.Close()
 			body, _ := io.ReadAll(r.Body)
-			key, err := setKey(string(body))
+			key, err := setKey(body)
 			if err == nil {
 				scheme := "http"
 				host := r.Host
@@ -66,6 +92,7 @@ func main() {
 				fmt.Fprint(w, url.String())
 				log.Printf("New content posted at key %s", key)
 			} else {
+				w.WriteHeader(http.StatusInternalServerError)
 				log.Print("Error when posting content: ", err)
 			}
 			return
@@ -77,13 +104,15 @@ func main() {
 			return
 		}
 
-		value, err := getKey(query)
-		if err == nil && value != "" {
-			if isUrl(value) {
-				http.Redirect(w, r, value, http.StatusFound)
+		value, numberOfReadBytes, err := getKey(query)
+		if err == nil && value != nil {
+			valueAsString := string(value[:numberOfReadBytes])
+			if isUrl(valueAsString) {
+				http.Redirect(w, r, valueAsString, http.StatusFound)
+				return
 			}
 
-			fmt.Fprint(w, value)
+			serveKey(query, w, r)
 			return
 		}
 
@@ -94,52 +123,56 @@ func main() {
 	http.ListenAndServe(":3000", nil)
 }
 
-func getKey(key string) (string, error) {
+func getKey(key string) ([]byte, int, error) {
 	if !isAlphanumeric(key) {
-		return "", errors.New("key is not alphanumeric")
+		return nil, 0, errors.New("key is not alphanumeric")
 	}
 
-	file, err := os.Open(DbFile)
+	file, err := os.Open(filepath.Join(dataPath, key))
 	if err != nil {
-		return "", err
-	}
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		text := scanner.Text()
-		if strings.HasPrefix(text, key+" ") {
-			return text[len(key+" "):], scanner.Err()
-		}
-	}
-
-	return "", scanner.Err()
-}
-
-func setKey(value string) (string, error) {
-	file, err := os.OpenFile(DbFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return "", err
+		return nil, 0, err
 	}
 	defer file.Close()
 
+	buffer := make([]byte, maxUrlLength)
+	numberOfReadBytes, err2 := file.Read(buffer)
+	if err2 != nil && err2 != io.EOF {
+		return nil, 0, err2
+	}
+
+	return buffer, numberOfReadBytes, nil
+}
+
+func serveKey(key string, w http.ResponseWriter, r *http.Request) {
+	if !isAlphanumeric(key) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	http.ServeFile(w, r, filepath.Join(dataPath, key))
+}
+
+func setKey(value []byte) (string, error) {
+	// Generate key
 	key := genKey()
 	for {
-		content, err := getKey(key)
+		_, _, err := getKey(key)
+
+		if errors.Is(err, os.ErrNotExist) {
+			break
+		}
+
 		if err != nil {
 			return "", err
 		}
 
-		if content == "" {
-			break
-		}
-
 		key = genKey()
 	}
-	_, err2 := file.WriteString(key + " " + value + "\n")
 
-	if err2 != nil {
-		return "", err2
+	// Write content to file
+	err := os.WriteFile(filepath.Join(dataPath, key), value, 0644)
+	if err != nil {
+		return "", err
 	}
 
 	return key, nil
